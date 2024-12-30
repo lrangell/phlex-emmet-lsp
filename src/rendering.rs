30c28d6822f9350ce6d87127c31bedd8a1c137a5
Lucas Rangel <lucas@rangel.dev>
2024-12-30 06:59:37 -0300

    feat: Basic rendering / expansion

---
+++ src/rendering.rs
@@ -0,0 +1,39 @@
+#![allow(unstable_name_collisions)]
+use crate::types::EmmetNode;
+use itertools::Itertools;
+
+pub trait Renderer {
+    fn render(&self) -> String;
+}
+impl Renderer for EmmetNode {
+    fn render(&self) -> String {
+        let classes = if self.classes.is_empty() {
+            None
+        } else {
+            Some(format!("class: {}", self.classes.iter().join(" ")))
+        };
+
+        let id = self.id.clone().map(|id| format!("id: {}", id));
+
+        let arguments: Vec<String> = [classes, id]
+            .into_iter()
+            .flatten()
+            .intersperse(", ".to_owned())
+            .collect();
+
+        let args = if arguments.is_empty() {
+            "".to_owned()
+        } else {
+            arguments.iter().join("")
+        };
+
+        format!(
+            "{tag}{arguments} {{ {children} }}
+             {siblings}",
+            tag = self.tag,
+            arguments = args,
+            children = self.children.iter().map(|n| n.render()).join("\n"),
+            siblings = self.siblings.iter().map(|n| n.render()).join("\n")
+        )
+    }
+}
